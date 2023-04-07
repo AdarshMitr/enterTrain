e7fc35a29c1ebb23aac21fd54d92310075e0d7d1
AdarshMitr <adarshmitr@gmail.com>
2023-04-07 21:37:41 +0530

    added Header and footer components

---
+++ enterTrain/src/components/Header/Header.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import "./Header.css";
+const Header = () => {
+  return (
+    
+      <span className="header">🎬🎥 EnterTrain 🚂 </span>
+
+  );
+};
+
+export default Header;
